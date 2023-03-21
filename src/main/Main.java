222792785fd43ed18f08437ad42a60c07234fcb3
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-21 19:28:12 -0300

    teste

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner grava = new Scanner(System.in);
+        int op=0;
+        
+        do{
+            
+            
+        }while(op != 5);
+    
+        
+    }
+    
+}
