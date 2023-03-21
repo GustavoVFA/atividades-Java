52c246d85e5b8d0f7da115d06e7c170c30d2a0bd
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-21 19:52:58 -0300

    Atv( - 1 a 4 - exercicios de logica
    )

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,14 +7,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner grava = new Scanner(System.in);
-        int op=0;
+        int cont=0, idade=0, soma=0, total=0;
         
-        do{
-            
-            
-        }while(op != 5);
-    
-        
-    }
+       /* while(cont<200){
+            cont++;
+            if(cont % 2 !=0) {
+                System.out.println(cont);
+            }
+        }
+    */
+            /*do{
+                System.out.println(cont);
+                cont--;
+            }while(cont>=0);
+        */
+            /*do{
+                System.out.println("informe a idade: ");
+                idade = grava.nextInt();
+                total++;
+                soma +=idade;
+            }while(idade!=0);
+            System.out.println(soma / total);
+            */
+            System.out.println("Informe a quantidade de Repetições: ");
+            cont = grava.nextInt();
+            while(soma < cont){
+                soma++;
+                System.out.println(soma +" Isso é um teste");
+            }
+}
     
 }
